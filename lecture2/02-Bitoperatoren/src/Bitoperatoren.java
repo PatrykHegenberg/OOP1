95ef9eecfea7f1718aaf1a47860e339a56b1bd25
Patryk Hegenberg <hegenberg@tixeltec.com>
2023-04-27 19:43:28 +0200

    lecture2 - finished all exercises except "Fingerübungen"

--- lecture2/02-Bitoperatoren/src/Bitoperatoren.java
+++ lecture2/02-Bitoperatoren/src/Bitoperatoren.java
@@ -22,6 +22,11 @@ public class Bitoperatoren {
         zweiKomplement(25);
 
         berecheneMitKomplement(100, 94);
+
+        // Vergleich zweier positiven ganzen Zahlen und Ausgabe an
+        // welcher Stelle sich die entsprechenden Binärzahlen unterscheiden
+
+        compareNumbers(4, 5);
     }
 
     public static int verdoppler(int zahl, int faktor) {
@@ -39,5 +44,19 @@ public class Bitoperatoren {
 
     public static void berecheneMitKomplement(int zahl1, int zahl2) {
         System.out.println(zahl1 + ~zahl2 + 1);
+    }
+
+     public static void compareNumbers(int zahl1, int zahl2) {
+        // Compare to zahl1 and zahl2 binary
+
+        int diff = zahl1 ^ zahl2; // XOR der beiden Zahlen
+        int bit = 1;
+        while(diff > 0) {
+            if((diff & 1) == 1) {
+            System.out.println("Die Zahlen unterscheiden sich an Stelle " + bit);
+            }
+        diff >>= 1;
+        bit++;
+        }
     }
 }
